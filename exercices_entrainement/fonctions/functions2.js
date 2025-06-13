914c62ca3e16520ad0908e9d4e8db22539832824
Niouk971 <florian.polter.77@gmail.com>
2025-06-13 14:48:21 +0200

    Modification function2.js

--- exercices_entrainement/fonctions/functions2.js
+++ exercices_entrainement/fonctions/functions2.js
@@ -39,17 +39,33 @@
 // console.log(hasKey('Mathilde'))
 // console.log(hasKey('Vi'))
 
-// soluce 2
+// soluce 2 (à compléter)
+
+// const peopleHasKey = ['Pierre', 'Laure', 'Mathilde', 'Vi'];
+
+// function hasKey(firstname){
+//     for(let i = 0; i < peopleHasKey.length; i ++){
+//         if (peopleHasKey[i] === firstname) {
+//             return `Est-ce que ${firstname} a les clés ? Oui.`
+//         }
+//         return `Est-ce que ${firstname} a les clés ? Non.`
+//     }
+// }
+
+// console.log(hasKey('Pierre'))
+// console.log(hasKey('Jerem'))
+// console.log(hasKey('Laure'))
+// console.log(hasKey('Mathilde'))
+// console.log(hasKey('Vi'))
+
+// soluce 3
 
 // function hasKey(firstname){
 //     let peopleHasKey = ['Pierre', 'Laure', 'Mathilde', 'Vi'];
-//     for(let i = 0; i < peopleHasKey.length; i ++){
-//         if(firstname === 'Pierre' || firstname === 'Laure' ||firstname === 'Mathilde' ||firstname === 'Vi'){
-//             return `Est-ce que ${firstname} a les clés ? Oui.`
-//         } else {
-//             return `Est-ce que ${firstname} a les clés ? Non.`
-//         }
+//     if(firstname === 'Pierre' || firstname === 'Laure' ||firstname === 'Mathilde' ||firstname === 'Vi'){
+//         return `Est-ce que ${firstname} a les clés ? Oui.`
 //     }
+//     return `Est-ce que ${firstname} a les clés ? Non.`
 // }
 
 // console.log(hasKey('Pierre'))
@@ -76,34 +92,69 @@
 
 // Et la fonction ne prend plus de paramètre mais renvoie la liste (sous forme de tableau) des personnes qui ont les clefs
 
-function hasKey(firstname){
-    let peopleHasKey = [
-        {
-            firstname: Pierre,
-            hasKey: true
-        },
-        {
-            firstname: Jerem,
-            hasKey: false
-        },
-        {
-            firstname: Laure,
-            hasKey: true
-        },
-        {
-            firstname: Mathilde,
-            hasKey: true
-        },
-        {
-            firstname: Vi,
-            hasKey: true
-        },
-    ]
-    if(peopleHasKey[{hasKey: true}]){
-        return "Yes"
-    } else {
-        return "No"
+// const peopleHasKey = [
+//     {
+//     firstname: "Pierre",
+//     hasKey: true,
+//     },
+//     {
+//     firstname: "Jerem",
+//     hasKey: false,
+//     },
+//     {
+//     firstname: "Laure",
+//     hasKey: true,
+//     },
+//     {
+//     firstname: "Mathilde",
+//     hasKey: true,
+//     },
+//     {
+//     firstname: "Vi",
+//     hasKey: true,
+//     }
+// ]
+
+// const pierre = { //objet
+//     firstname: "Pierre",
+//     hasKey: true,
+// }
+
+// console.log(pierre.firstname, pierre.hasKey)
+
+// const pierre = ["Pierre", true, "Paris", false];
+// const samir = ["Samir", false, "Paris", true];
+// const users = [pierre, samir];
+
+// for(let i = 0; i < users.length; i ++) {
+//     console.log(users[0][0], users[1][0]);
+// }
+
+// exemple restaurant
+
+const restaurant = {
+    name: "Mcdo",
+    menus: ["McFirst", "Best of", "Maxi Best of", "McWrap"],
+}
+
+// console.log(`Le restaurant ${restaurant.name} propose le menu ${restaurant.menus[0]}`);
+
+// for(let i = 0; i < restaurant.menus.length; i ++){
+//     console.log(`Le restaurant ${restaurant.name} propose les menus ${restaurant.menus[i]}`);
+// }
+
+console.log(`Le restaurant ${restaurant.name} propose ${restaurant.menus.length} menus :`);
+
+let numberMenus = 4
+
+function menusMcdo(numberMenus) {
+    numberMenus = 0
+    for(let i = 0; i < restaurant.menus.length; i ++) {
+        if (a) {
+            return restaurant.menus
     }
 }
 
-console.log(hasKey('Pierre'))
+console.log(`Le restaurant ${restaurant.name} propose ${restaurant.menus.length} menus.
+Les menus sont les suivants : ${menusMcdo(0)}`);
+}
