293bd462e7799084dd4d4b8b51d95af477ca3921
Niouk971 <florian.polter.77@gmail.com>
2025-07-08 15:51:36 +0200

    Ajout dossier boucles et finalisation exercice1.js

---
+++ exercices_entrainement/boucles/exercice1.js
@@ -0,0 +1,15 @@
+let compteur = 1
+
+// for (compteur; compteur <= 99; compteur++) {
+//     console.log(compteur);
+// }
+
+// while (compteur < 99) {
+//     compteur ++
+//     console.log(compteur);
+// }
+
+for (compteur; compteur <= 49; compteur++) {
+    console.log(compteur);
+}
+
