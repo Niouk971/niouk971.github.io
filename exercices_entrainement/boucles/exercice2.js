c86c80ebaffc0b69c6c3b4f353a88f601abf617c
Niouk971 <florian.polter.77@gmail.com>
2025-07-09 11:42:47 +0200

    finalisation exercice2.js

---
+++ exercices_entrainement/boucles/exercice2.js
@@ -0,0 +1,24 @@
+// Partie 1
+
+// let letters = ['A', 'd', 'a', '-', 'T', 'e', 'c', 'h'];
+
+// let message = "";
+
+// for (let i = 0; i < letters.length; i ++) {
+//     message += letters[i]
+// }
+
+// console.log(message)
+
+
+// Partie 2
+
+let letters = [ 'A', 'd', 'a', '-', 'S', 'c', 'h', 'o', 'o', 'l']
+
+let message = "";
+
+for (let i = 0; i < letters.length; i ++) {
+    message = letters.join('')
+}
+
+console.log(message)
