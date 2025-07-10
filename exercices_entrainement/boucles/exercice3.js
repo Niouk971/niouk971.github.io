5aca1d959b70d361fb6e873303a17db5542bd5be
Niouk971 <florian.polter.77@gmail.com>
2025-07-10 09:58:27 +0200

    Finalisation exercice3.js

---
+++ exercices_entrainement/boucles/exercice3.js
@@ -0,0 +1,30 @@
+// soluce 1
+
+// let nombres = prompt("Choisis un nombre entre 0 et 10 :")
+
+// while (nombres < 0 || nombres > 10) {
+//     nombres = prompt("Erreur. Choisis un autre chiffre.")
+// }
+
+// for (let i = nombres; i >= 0; i -- ) {
+//     alert(i)
+// }
+
+
+// soluce 2
+
+//Nous souhaitons créer un décompte.
+//Demande à l'utilisateur (à l'aide de prompt) un nombre entre 0 et 10.
+//Affiche ensuite tous les nombres sous le nombre indiqué par l'utilisateur (s'il rentre 4, on affichera 4, 3, 2, 1, 0).
+//Attention, si l'utilisateur rentre un chiffre erroné il faudra afficher message d'erreur.
+
+let nombre = prompt("Entre un nombre entre 0 et 10");
+
+if (nombre >= 0 && nombre <= 10) {
+    for (let i = nombre; i >= 0; i--) {
+        console.log(i);
+        prompt(i)
+    }
+} else {
+    alert("Erreur : tu dois entrer un nombre entre 0 et 10");
+}
