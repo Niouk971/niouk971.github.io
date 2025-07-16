ecb2d2956454bf0c028f1afcd4fc20ac44ab450d
Niouk971 <florian.polter.77@gmail.com>
2025-07-16 15:52:53 +0200

    Finalisation exercice 1

--- exercices_entrainement/async/exercice1.js
+++ exercices_entrainement/async/exercice1.js
@@ -1,8 +1,22 @@
+const offersList = document.querySelector('#offers');
+
 async function fetchOffers() {
     const response = await fetch('https://www.codepassport.dev/api/offers'); // on appelle fetch avec une URL d'API
     const offers = await response.json();
     // console.log(response); // affiche la promesse retournée par fetch  
-    console.log(offers);
+    // console.log(offers);
+
+    offers.forEach(element => {
+
+        const showTitle = document.createElement('h2');
+        offersList.appendChild(showTitle);
+        showTitle.innerHTML = element.titre;
+
+        const showDescription = document.createElement('p');
+        offersList.appendChild(showDescription);
+        showDescription.innerHTML = element.description
+
+    });
 }
 
 fetchOffers();
